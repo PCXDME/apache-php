6e2820a995c82c77ca7c8b4c30c4390add77bb9b
Peeranut Chindanonda <pcxd@me.com>
2016-10-21 15:27:12 +0700

    Change user

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
     unset ALLOW_OVERRIDE
 else
     sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+    sed -i -e '/export APACHE_RUN_USER=/ s/=.*/=bin/' /etc/apache2/envvars
+    sed -i -e '/export APACHE_RUN_GROUP=/ s/=.*/=bin/' /etc/apache2/envvars
     a2enmod rewrite
     a2enmod headers
 fi
