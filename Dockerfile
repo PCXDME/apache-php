ddb3c84364f03f92b747ca72114f5b160300b55e
Peeranut Chindanonda <PCXD@live.com>
2016-11-19 21:32:08 +0700

    Update maintainer and enable ssl

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:trusty
-MAINTAINER Fernando Mayo <fernando@tutum.co>
+MAINTAINER pcxd
 
 # Install base packages
 RUN apt-get update && \
--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ else
     sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
     sed -i -e '/export APACHE_RUN_USER=/ s/=.*/=bin/' /etc/apache2/envvars
     sed -i -e '/export APACHE_RUN_GROUP=/ s/=.*/=bin/' /etc/apache2/envvars
+	a2ensite default-ssl
+	a2enmod ssl
     a2enmod rewrite
     a2enmod headers
 fi
